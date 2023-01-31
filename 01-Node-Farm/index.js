4ab509c60d54936bd103dbdd9aa311ca704b86e8
Aakash <aakash.codes02@gmail.com>
2023-01-31 12:03:21 +0530

    simple routing with URL module

--- 01-Node-Farm/index.js
+++ 01-Node-Farm/index.js
@@ -1,12 +1,25 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 // FILES
 
 // SERVER
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!');
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('Hello from the OVERVIEW!');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.writeHead('404', {
+      'Content-type': 'text/html',
+      'my-own-header': 'Hey-there',
+    });
+    res.end('<h1>Sorry for inconvenience, Page not Found!!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
