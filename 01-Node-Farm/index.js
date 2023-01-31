ddafaf191607b9d05b1093e05cc55b25252906fb
Aakash <aakash.codes02@gmail.com>
2023-01-31 10:38:24 +0530

    created a simple web server with node.js

--- 01-Node-Farm/index.js
+++ 01-Node-Farm/index.js
@@ -1,27 +1,14 @@
+const http = require('http');
 const fs = require('fs');
 
-// Blocking, synchronous way
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+// FILES
 
-const textOut = `All about Avacado: ${textIn}.\n Created on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written!');
+// SERVER
 
-// Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR! 💥');
-
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
-
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Files has been written 🏃‍♂️');
-      });
-    });
-  });
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server!');
 });
 
-console.log('Will read this');
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Server is running');
+});
